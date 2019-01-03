a7c232e75c85811781d37cb73ba72ce2aaf52993
Rangnau Vadim <vadim.rangnau@itw-ocw.com>
2019-01-03 10:34:31 +0100

    first figures

--- figures.h
+++ figures.h
@@ -6,6 +6,29 @@ class figures
 {
 public:
     figures();
+    virtual bool firstMoveOther;
+    virtual int x_Movement; // can't move in X
+    virtual int y_Movement; // can't move in Y
+    virtual int fightMovement; //spezific fight direction
 };
+
+class solider : figures
+{
+public:
+    bool firstMoveOther = true;
+    int Y_Movement = 1; // can move 1 field in Y
+    int fightMovement = 1; // can fight in X+1 and Y+1
+};
+
+class horse : figures
+{
+
+};
+
+
+
+
+
+
 
 #endif // FIGURES_H
